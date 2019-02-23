503f9bce2a454cbf69856306e177a68cbe89ee4a
caren <caren.ruiz@ecloudsolutions.com>
2019-02-23 20:51:51 -0300

    CUSTOMERS

--- src/componentes/Cliente.js
+++ src/componentes/Cliente.js
@@ -4,8 +4,18 @@ import Class from './Agregar';
 
 const Cliente = (props) => {
     const {imagen,nombre,direccion,id,descripcion} = props.cliente;
-
-    return nombre;
+    return (
+        <div className="mdc-card">
+            <ul>
+                <li>Nombre mascota: {nombre}</li>
+                <li>Nombre dueno:</li>
+                <li>Descpcion: {descripcion}</li>
+                <li>Direccion: {direccion}</li>
+                <li>Id: {id}</li>
+                <li><img src={"../images/img/"+ imagen +".jpg"} alt="mascota"/></li>
+            </ul>
+        </div>
+    );
 }
 
 
